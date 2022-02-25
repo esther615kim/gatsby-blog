93cca80d3d361c763e32d13755062c4c31d42589
esther615kim <vilchen@naver.com>
2022-02-26 07:22:43 +0900

    style: add responsive layout

--- Desktop/ts-portfolio/src/components/Side.tsx
+++ Desktop/ts-portfolio/src/components/Side.tsx
@@ -6,40 +6,40 @@ import {
   AiFillCodeSandboxCircle,
 } from "react-icons/ai";
 import { GoLocation } from "react-icons/go";
-import glass from "../images/glass.png";
+
 const Side = () => {
   return (
     <div>
-      <img
-        className="mx-auto rounded-full w-30 h-30"
-        src={glass}
-        alt="profile"
-      />
-      <h3 className="my-2 text-2xl font-semibold tracking-wider">Esther Kim</h3>
-      <p className="px-2 py-1 mx-4 mt-4 font-semibold bg-red-200 rounded-md">
-        Front End Developer
-      </p>
-      <p className="px-2 py-1 mx-4 mt-4 font-semibold rounded-md bg-gradient-to-r from-stone-400 to-stone-300">
-        CV
-      </p>
+
+      <h3 className="lg:mt-20 lg:pt-40 mt-10 text-3xl font-semibold tracking-wider font-oohhbaby">Esther Kim</h3>
+      <p className="font-poppins mt-5 mb-20 lg:mb-40 font-medium text-lg ">Front End Developer</p>
+      {/* <p className="px-2 py-1 mx-4 mt-4 font-semibold bg-stone-100 rounded-md"> */}
+        {/* Front End Developer */}
+      {/* </p> */}
 
       {/* socials */}
-      <div className="flex justify-around my-5 text-red-500 md:w-auto md:2-full">
+      <div className="flex justify-around pb-5 px-20 my-5 text-stone-100 md:w-auto md:2-full">
         {/* <Link to="https://github.com/esther615kim"> */}
         <AiOutlineLinkedin className="w-6 h-6 cursor-pointer" />
         {/* </Link> */}
         <AiFillGithub className="w-6 h-6 cursor-pointer" />
         <AiFillCodeSandboxCircle className="w-6 h-6 cursor-pointer" />
       </div>
-      <div className="py-3 my-5 bg-stone-200">
-        <div className="flex items-center justify-center space-x-1">
+      {/* <div className="py-3 my-5 bg-stone-200"> */}
+      <div className="py-3 my-5 bg-white">
+        <div className="flex items-center justify-center space-x-1 space-y-3">
           <GoLocation className="w-5 h-5" />
-          <span>Rugby, UK</span>
+          <span className="block font-rubik">Rugby, UK</span>
         </div>
-        <p className="my-2">vilchen15@gmail.com</p>
-        <p className="my-2">0739944444</p>
+        <p className="my-2 font-rubik">vilchen15@gmail.com</p>
       </div>
-      <button className="w-10/12 py-1 my-1 text-white rounded-md mb-14 bg-gradient-to-r from-red-300 to-blue-400">Contact Me</button>
+      {/* <p className="px-2 py-1 mx-4 mt-4 font-semibold rounded-md bg-gradient-to-r from-stone-400 to-stone-300">
+        CV
+      </p> */}
+      <div className="space-y-2 pb-10 lg:pb-20">
+            <button className=" font-poppins w-10/12 py-1 my-1 text-white rounded-md mb-2 bg-gradient-to-r from-red-300 to-blue-400">CV</button>
+      <button className=" font-poppins w-10/12 py-1 my-1 text-white rounded-md bg-gradient-to-r from-red-300 to-blue-400">Contact Me</button>
+      </div>
     </div>
   );
 };
