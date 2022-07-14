f2994f37a8fe34ade96d1120d82528af1239a520
Esther Kim <Esther.Kim@christie.com>
2022-07-14 17:15:00 +0100

    add gatsby-source-transformer-csv

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -20,20 +20,28 @@ const config: GatsbyConfig = {
     options: {
       "icon": "src/images/icon.png"
     }
-  }, "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+  }, "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp","gatsby-transformer-csv", {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "images",
       "path": "./src/images/"
     },
     __key: "images"
-  }, {
+  },{
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "pages",
       "path": "./src/pages/"
     },
     __key: "pages"
+  }, 
+  {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "data",
+      "path": "./src/data/"
+    },
+    __key: "data"
   }]
 };
 
