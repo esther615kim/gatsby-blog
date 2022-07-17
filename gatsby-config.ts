119526582f76dd079c2826f878cdf23a4bc4c0c8
esther615kim <vilchen@naver.com>
2022-07-17 19:19:31 +0900

    add tailwind css setup

---
+++ gatsby-config.ts
@@ -0,0 +1,48 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `My Gatsby Blog`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+  // If you use VSCode you can also use the GraphQL plugin
+  // Learn more at: https://gatsby.dev/graphql-typegen
+  graphqlTypegen: true,
+  plugins: [{
+    resolve: 'gatsby-source-contentful',
+    options: {
+      "accessToken": "8Git6JcO73p40t9fwk-5_7GDdHI-ykm-Gr8qvzlKAAo",
+      "spaceId": "0yn2qh1q6m2r"
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/images/icon.png"
+    }
+  }, "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp","gatsby-transformer-csv", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  },{
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "pages",
+      "path": "./src/pages/"
+    },
+    __key: "pages"
+  }, 
+  {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "data",
+      "path": "./src/data/"
+    },
+    __key: "data"
+  }]
+};
+
+export default config;
---
+++ mydocs/blog.md
@@ -0,0 +1,3 @@
+### Gatsby & TypeScript
+
+- `3 Overview of Gatsby Concepts` https://velog.io/write?id=2508208f-86ca-45fb-8bd1-93893229ad61
